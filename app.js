bba7c46c0f3e5e0e50aafd3ad40aac4468b5e6cf
vishal9643 <amitkrmishra864@gmail.com>
2023-06-20 20:00:27 +0530

    cors-error

--- app.js
+++ app.js
@@ -18,12 +18,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 // app.use(cors());
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 // app.use(cookieParser());
 
