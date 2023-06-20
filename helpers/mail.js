46cbb5296805ae838b6917b765d826a7abe59117
vishal9643 <amitkrmishra864@gmail.com>
2023-06-20 22:38:31 +0530

    mail

--- helpers/mail.js
+++ helpers/mail.js
@@ -14,7 +14,7 @@ const mailer = async (info) => {
   });
 
   // send mail with defined transport object
-  const info = await transporter.sendMail({ info });
+  const info = await transporter.sendMail(info);
 
   console.log("Message sent: %s", info.messageId);
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
