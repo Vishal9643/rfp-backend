6db1084a8052a397f7cf74ff42b0d79ce7fae33b
vishal9643 <amitkrmishra864@gmail.com>
2023-06-21 14:00:59 +0530

    changes in approved vendor

--- helpers/approvendor_validation.js
+++ helpers/approvendor_validation.js
@@ -1,9 +1,11 @@
 const joi = require("@hapi/joi");
 
-const authSchema = joi.object({
-  user_id: joi.number(),
-  status: joi.string(),
-});
+const authSchema = joi
+  .object({
+    user_id: joi.number(),
+    status: joi.string(),
+  })
+  .unknown();
 
 module.exports = {
   authSchema4: authSchema,
